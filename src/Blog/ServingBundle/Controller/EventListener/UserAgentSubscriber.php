f1646da2a931cc8c1d6e7662cf509489fc595494
luciana_hembert <contact@lucianahembert.com>
2017-02-20 07:27:48 +0100

    replacing the _controller

--- src/Blog/ServingBundle/Controller/EventListener/UserAgentSubscriber.php
+++ src/Blog/ServingBundle/Controller/EventListener/UserAgentSubscriber.php
@@ -4,10 +4,10 @@ namespace Blog\ServingBundle\Controller\EventListener;
 
 use \Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use \Psr\Log\LoggerInterface;
-use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 
 
 class UserAgentSubscriber implements EventSubscriberInterface
@@ -33,6 +33,11 @@ class UserAgentSubscriber implements EventSubscriberInterface
             //$response = new Response('Come back later');
             //$event->setResponse($response);
 //        }
+        
+//        $request->attributes->set('_controller', function($id) {
+//            return new Response('Hello Dinosaur! id : ' . $id);
+//        });
+    
     }
     
     public static function getSubscribedEvents() {
@@ -40,7 +45,8 @@ class UserAgentSubscriber implements EventSubscriberInterface
         // constant that means kernel.request
     
         return array(
-            KernelEvents::REQUEST => 'onKernelRequest'
+            KernelEvents::REQUEST => 'onKernelRequest',
+            //KernelEvents::CONTROLLER => 'onKernelRequest',
         );
     }
 
